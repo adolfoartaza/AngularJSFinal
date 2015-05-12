27a0899afb16b84f71ef2929fd8664e46976b547
adolfoartaza <adolfoartaza@gmail.com>
2015-05-12 01:38:48 -0400

    Final Complete
    Works properly - no CSS styling added due to time constraints!

---
+++ app.js
@@ -0,0 +1,64 @@
+var app = angular.module('HomeworkApp', ['ui.router']);
+
+app.controller('homeworkController', function($scope, hwkFactory) {
+    $scope.homeworks = hwkFactory;
+    $scope.disp = function (homework){
+        homework.display = !homework.display;
+    }
+});
+
+app.factory('hwkFactory', function () {
+    return [
+        {
+            assignment: 'Test First Javascript',
+            url: 'http://github.com/adolfoartaza/FullstackTestFirst',
+            status: 'completed',
+            display: false
+        },
+        {
+            assignment: 'Workshop 1',
+            url: 'http://github.com/adolfoartaza/flashcards-day1',
+            status: 'completed',
+            display: false
+
+        },
+        {
+            assignment: 'Workshop 2',
+            url: 'http://github.com/adolfoartaza/flashcards',
+            status: 'completed',
+            display: false
+        },
+        {
+            assignment: 'Workshop 3',
+            url: 'http://github.com/adolfoartaza/flashcards-day1',
+            status: 'incomplete',
+            display: false
+        },
+        {
+            assignment: 'Workshop 4',
+            url: 'http://github.com/adolfoartaza/flashcards-day1',
+            status: 'incomplete',
+            display: false
+        },
+        {
+            assignment: 'Final Assignment',
+            url: 'http://github.com/adolfoartaza/flashcards-day1',
+            status: 'completed',
+            display: false
+        }
+    ]
+});
+
+app.config(function($stateProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'index.html',
+        controller: 'homeworkController'
+    })
+
+    $stateProvider.state('about', {
+        url: '/about',
+        templateUrl: 'about.html',
+        controller: 'homeworkController'
+    })
+})
